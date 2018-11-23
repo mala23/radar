9bd11202346db1602be94f92b2e9d4c74181071e
Colin Nico Schmid <mail@mala23.me>
2018-11-23 12:53:45 +0100

    merge and clean

--- data/merge_wind.py
+++ data/merge_wind.py
@@ -1,6 +1,21 @@
 import numpy as numpy
 import pandas as pandas
 
+# import data csv, rename and rearrange headers
 data = pandas.read_csv('./datasets/wind_tidy.csv')
+data.columns = ['Site', 'Startup', 'Production', 'Location', 'Canton', 'ZE-Coordinates']
+data = data[['Location','Production','Startup']]
 
 print(data)
+
+# import coordinates csv
+coordinates = pandas.read_csv('./coordinates/coordinates_wind.csv')
+
+print(coordinates)
+
+# merge
+merged = pandas.merge(data, coordinates, how='outer', right_on='Location', left_on='Location')
+#merged = merged[['Location','Production','Startup','Lat','Lng']]
+merged.drop(merged.columns[[5]], axis=1, inplace=True)
+merged.drop_duplicates(inplace=True)
+print(merged)
