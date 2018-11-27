1bdb485efb4d7484ea0dff56330e2b1fd801ad7b
Colin Nico Schmid <mail@mala23.me>
2018-11-27 10:21:08 +0100

    exit the encoding hell

---
+++ data/merge_water.py
@@ -0,0 +1,19 @@
+import numpy as numpy
+import pandas as pandas
+
+# import data csv, rename and rearrange headers
+data = pandas.read_csv('./datasets/water.csv')
+#print(data)
+
+# import coordinates csv
+coordinates = pandas.read_csv('./coordinates/coordinates_water.csv')
+#print(coordinates)
+
+# merge
+merged = pandas.merge(data, coordinates, how='outer', right_on='Location', left_on='Location')
+merged.drop(merged.columns[[4]], axis=1, inplace=True)
+merged.drop_duplicates(inplace=True)
+#print(merged)
+
+# save to csv
+merged.to_csv(path_or_buf='./water2.csv', index=False)
